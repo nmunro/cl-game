77c0a8df7246d073d5dc811d0554f938c8e7c48d
Neil Munro <neilmunro@gmail.com>
2019-04-14 10:54:42 +0100

    [#1] Implemented defense function

--- main.lisp
+++ main.lisp
@@ -2,6 +2,7 @@
   "Create an entity in the game"
   (let ((entity (make-hash-table)))
     (setf (gethash 'name entity) name)
+    (setf (gethash 'exp entity) 0)
     (setf (gethash 'level entity) 1)
     (setf (gethash 'atk entity) 10)
     (setf (gethash 'def entity) 10)
@@ -27,7 +28,23 @@
   (let ((dmg (- (+ amount (gethash 'atk e1)) (gethash 'def e2))))
     (format
       t
-      "~A hit ~A for DMG:~A with DEF:~A~%"
+      "You attack: ~A hits ~A for DMG:~A with DEF:~A~%"
+      (gethash 'name e1)
+      (gethash 'name e2)
+      dmg
+      (gethash 'def e2))
+
+      ;; Ensure negative numbers aren't subtracted, else health
+      ;; will actually go up!
+      (if (> dmg 0)
+        (setf (gethash 'hp e2) (- (gethash 'hp e2) dmg)))))
+
+(defun defend (e1 e2 amount)
+  "Function to defend against an entity attack"
+  (let ((dmg (- (+ amount (gethash 'atk e1)) (* 2 (gethash 'def e2)))))
+    (format
+      t
+      "You defend: ~A hits ~A for DMG:~A with DEF:~A~%"
       (gethash 'name e1)
       (gethash 'name e2)
       dmg
@@ -41,18 +58,25 @@
 (defun is-alive? (entity)
   (> (gethash 'hp entity) 0))
 
+(defun user-input ()
+  (format t "Please enter a command: ")
+  (let ((cmd (string-downcase (read-line))))
+    cmd))
+
 (defun game-loop (player entity)
-  (display-entity player)
-  (display-entity entity)
+  (format
+    t
+    "Welcome ~A, you encounter a Goblin, it attacks.~%What do you do? "
+    (gethash 'name player))
 
-  (attack entity player (random 25))
+    (let ((action (user-input)))
+      (cond
+        ((equal action "attack") ((lambda ()
+          (attack player entity (random 25)) (display-entity player))))
 
-  (if (is-alive? player)
-    (attack player entity (random 25)))
+        ((equal action "defend") ((lambda ()
+          (defend player entity (random 25)) (display-entity player))))
 
-  (cond
-    ((not (is-alive? player)) (format t "Game Over: You Died~%"))
-    ((not (is-alive? entity)) (format t "Game Over: You Won~%"))
-    ((and (is-alive? player) (is-alive? entity)) (game-loop player entity))))
+        (t "Dunno"))))
 
 (game-loop (create-player) (create-entity "Gobin"))
